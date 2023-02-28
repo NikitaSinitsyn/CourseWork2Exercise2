27b263ed60ae26e3c9895df1fd9f93ee663f8b78
Nikita <nikita.sinitsyn.lipetsk@abv.bg>
2023-02-28 23:03:02 +0200

    Exercise 2 done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите текст:");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine().toLowerCase();
+        String[] words = text.split("\\W+");
+
+
+        long wordCount = words.length;
+        System.out.printf("В тексте %d слов%n", wordCount);
+
+
+        Map<String, Long> wordFrequencies = Arrays.stream(words)
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        List<Map.Entry<String, Long>> topWords = wordFrequencies.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .collect(Collectors.toList());
+
+        System.out.println("TOP10:");
+        topWords.forEach(e -> System.out.printf("%d — %s%n", e.getValue(), e.getKey()));
     }
 }
